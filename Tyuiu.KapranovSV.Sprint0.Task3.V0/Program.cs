07634e267c5c27abcaeaae8a27f2c16dbc6b8bc8
Kapranovsv <sergeirunik@yandex.ru>
2025-09-11 00:36:59 +0500

    Task3

---
+++ Tyuiu.KapranovSV.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.KapranovSV.Sprint0.Task3.V0.Lib;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7,7));
+        Console.ReadLine();
+    }
+}
