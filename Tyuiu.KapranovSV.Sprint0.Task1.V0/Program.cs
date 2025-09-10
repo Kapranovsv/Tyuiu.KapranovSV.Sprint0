312bf95f9b2bca9a3a18cb894ae562730de552bd
Kapranovsv <sergeirunik@yandex.ru>
2025-09-10 23:09:10 +0500

    Task1

---
+++ Tyuiu.KapranovSV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Сергей";
+        int age = 19;
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
